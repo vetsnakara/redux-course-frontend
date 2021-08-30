ce180a73701af6f01985f7f223d5768beeee7024
Konstantin Arakantsev <kostya3312@gmail.com>
2021-08-30 10:14:06 +0300

    basic redux structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+import store from "./store"
+
+import * as actions from "./actions"
+
+const unsubscribe = store.subscribe(() => {
+    console.log("Store changed", store.getState())
+})
+
+store.dispatch(actions.bugAdded({ description: "bug 1" }))
+
+store.dispatch(actions.bugRemoved({ id: 0 }))
