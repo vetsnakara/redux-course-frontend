07c55dc6907334c0939382e58dd94bccf1c97acb
Konstantin Arakantsev <kostya3312@gmail.com>
2021-08-30 18:08:06 +0300

    redux devtools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import { createStore } from "./customStore"
+import { createStore } from "redux"
+import { devToolsEnhancer } from "redux-devtools-extension"
 
 import { reducer } from "./reducer"
 import * as actions from "./actions"
 
-const store = createStore(reducer)
+const store = createStore(reducer, devToolsEnhancer({ trace: true }))
 
 store.subscribe(() => console.log(store.getState()))
 
 store.dispatch(actions.bugAdded({ description: "Bug 1" }))
-
-console.log(store.getState())
+store.dispatch(actions.bugAdded({ description: "Bug 2" }))
+store.dispatch(actions.bugAdded({ description: "Bug 3" }))
+store.dispatch(actions.bugResolved({ id: 0 }))
