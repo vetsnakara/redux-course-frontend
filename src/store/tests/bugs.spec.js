5182570eef800899ecd6a5f654ded2df33e520da
Konstantin Arakantsev <kostya3312@gmail.com>
2021-09-01 13:12:19 +0300

    refactor test

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,22 +1,40 @@
 import axios from "axios"
 import MockAdapter from "axios-mock-adapter"
 
-import { addBug } from "../bugs"
 import configureStore from "../configureStore"
+import { addBug } from "../bugs"
 
 describe("bugsSlice", () => {
-    it("should handle the adding bug", async () => {
-        const fakeAxios = new MockAdapter(axios)
+    let fakeAxios
+    let store
 
+    beforeEach(() => {
+        fakeAxios = new MockAdapter(axios)
+        store = configureStore()
+    })
+
+    it("should save bug in the store if bug successfully saved on server", async () => {
         const bug = { description: "a" }
         const savedBug = { ...bug, id: 1 }
 
         fakeAxios.onPost("/bugs").reply(200, savedBug)
 
-        const { dispatch, getState } = configureStore()
+        await store.dispatch(addBug(bug))
 
-        await dispatch(addBug(bug))
-
-        expect(getState().entities.bugs.list).toContainEqual(savedBug)
+        expect(bugsSlice().list).toContainEqual(savedBug)
     })
+
+    it("should NOT save bug in the store if bug NOT successfully saved on server", async () => {
+        const bug = { description: "a" }
+
+        fakeAxios.onPost("/bugs").reply(500)
+
+        await store.dispatch(addBug(bug))
+
+        expect(bugsSlice().list).toHaveLength(0)
+    })
+
+    function bugsSlice() {
+        return store.getState().entities.bugs
+    }
 })
